2accb2b3261afdc7b20b720fd26dd1faaf87ecff
Jaydeep Dave <jaydeep.dave@volansys.com>
2020-04-08 10:57:02 +0530

    Comment!

--- Script.py
+++ Script.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 user_name = 'jaydeep.dave@volansys.com'
 password = 'Hello@123'
-browser = webdriver.Chrome(r"E:\\chromedriver.exe")
+browser = webdriver.Chrome(r"E:\\chromedriver.exe") #location of webdriver
 browser.get('https://dashboard-dev.aylanetworks.com/')
 browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div/div[1]/div[2]/div/div[2]/div/div[3]/button[2]').click()
 email_element = browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div/div[1]/div[3]/div/div[1]/form/ul/li[1]/input')
