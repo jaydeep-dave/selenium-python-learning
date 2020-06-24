95a8f85d8b0ece7280b6775febc20187229ce8ac
Jaydeep Dave <jaydeep.dave@volansys.com>
2020-06-24 22:21:19 +0530

    Latest - 24th June 2020

--- Script_Amazon.py
+++ Script_Amazon.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome(r"E:\\chromedriver.exe") #location of webdriver
 browser.get('https://www.amazon.in/')
+search_text = "One Plus 8"
+browser.find_element_by_xpath(".//input[@id='twotabsearchtextbox']").send_keys(search_text)
+browser.find_elements_by_xpath(".//div[@class='nav-search-submit nav-sprite']//input[@class='nav-input']").click()
 
 
 
