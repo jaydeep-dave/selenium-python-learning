00b54567f859f2378d8331c1addf4bb29cc62113
Jaydeep Dave <jaydeep.dave@volansys.com>
2020-06-24 23:04:34 +0530

    Latest - 24th June 2020

--- Script_Amazon.py
+++ Script_Amazon.py
@@ -1,16 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 browser = webdriver.Chrome(r"E:\\chromedriver.exe") #location of webdriver
 browser.get('https://www.amazon.in/')
-search_text = "One Plus 8"
-browser.find_element_by_xpath(".//input[@id='twotabsearchtextbox']").send_keys(search_text)
-browser.find_elements_by_xpath(".//div[@class='nav-search-submit nav-sprite']").click()
+search_text = "One Plus 8" #enter your search text here
+search = browser.find_element_by_xpath(".//input[@id='twotabsearchtextbox']").send_keys(search_text)
+browser.find_element_by_xpath(".//div[@class='nav-search-submit nav-sprite']//input[@class='nav-input']").click()
 
-
-
-
-
-#keep browser open
-#chrome_options = Options()
-#chrome_options.add_experimental_option("detach", True)
+element = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//span[contains(text(),'OnePlus 8 5G (Glacial Green 6GB RAM+128GB Storage)')]"))
+    )
+element.click()
