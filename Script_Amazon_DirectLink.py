3c02f169b8827c608436c5909caca17b4fe56b2e
Jaydeep Dave <jaydeep.dave@volansys.com>
2020-08-09 10:52:43 +0530

    Latest commit!

--- Script_Amazon_DirectLink.py
+++ Script_Amazon_DirectLink.py
@@ -3,6 +3,7 @@ from webdriver_manager.chrome import ChromeDriverManager #https://pypi.org/proje
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.keys import Keys
 import time
 
 browser = webdriver.Chrome(ChromeDriverManager().install()) 
@@ -18,6 +19,15 @@ if price > desired_price:
     print("Price is greater than your desired price. Actual price is: ₹",price,"which is ₹",(price - desired_price),"more than your desired price.")
 else:
     print("Price is less than your desired price. Actual price is: ₹",price,"which is ₹",(desired_price - price),"less than your desired price.")
+    browser.find_element_by_xpath(".//input[@id='buy-now-button']").click()
+
+    #enter email
+    email = '' #enter your email address here
+    password = '' #enter your password here
+    email_element = browser.find_element_by_xpath(".//input[@id='ap_email']").send_keys(email)
+    browser.find_element_by_xpath(".//input[@id='continue']").click()
+    password_element = browser.find_element_by_xpath(".//input[@id='ap_password']").send_keys(password)
+    browser.find_element_by_xpath(".//input[@id='signInSubmit']").click()
 
 time.sleep(3)
 browser.quit()
